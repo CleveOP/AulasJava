4ad920b328556a1781c2479e6ae2f51971f5341c
Cleverson de Oliveira Pereira <cleversonpereir4@gmail.com>
2025-08-20 18:15:04 -0300

    Primeiro commit do projeto Hello

---
+++ Hello.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Hello {
+    public static void main(String[] args) {
+        double numero1, numero2;
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Digite o primeiro numero: ");
+    numero1 = scanner.nextDouble();
+    
+    System.out.println("Digite o segundo numero: ");
+    numero2 = scanner.nextDouble();
+
+    double resultado = (numero1 + numero2) ;
+
+    System.out.println("O terceiro numero sera: " + resultado ) ;
+    scanner.close();
+
+
+    
+
+
+
+
+        }
+    }
